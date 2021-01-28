b6d91257e261af37e996389e2f40b4f2ce838e31
yuvan-adithiya <74665130+yuvan-adithiya@users.noreply.github.com>
2021-01-28 18:15:40 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,22 @@
+var canvas;
+var database;
+var gameState = 0, contestantCount, quiz, question, person, allPeople;
+var backgroundColor = "pink";
+
+
+function setup(){
+  database = firebase.database();
+  canvas = createCanvas(850,400);
+  quiz = new Quiz();
+  quiz.getState();
+  quiz.start();
+}
+
+
+function draw(){
+  background(backgroundColor);
+
+  
+
+}
+
